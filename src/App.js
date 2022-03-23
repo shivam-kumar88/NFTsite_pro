3a25591337916a332941cbdbba776c9039ec4a47
shivam <shivam882531@gmail.com>
2022-03-23 13:24:08 +0530

    all updated

--- src/App.js
+++ src/App.js
@@ -4,8 +4,36 @@ import pic from './images/logo.png'
 import { MdNotes } from "react-icons/md"
 import { IoAdd } from "react-icons/io5";
 import Product_Card from './Components/Product_Card';
+import pic1 from './images/pic1.png'
+import pic2 from './images/pic2.png'
+import pic3 from './images/pic3.png'
+import pic4 from './images/pic4.png'
+
 
 const rarity = ['Common', 'Elite', 'MithiCal', 'Legendery']
+
+const cards = [
+  {
+    picture:pic1,
+    title:"card1",
+    rarity:'MithiCal'
+  },
+  {
+    picture:pic2,
+    title:"card1",
+    rarity:'MithiCal'
+  },
+  {
+    picture:pic3,
+    title:"card1",
+    rarity:'MithiCal'
+  },
+  {
+    picture:pic4,
+    title:"card1",
+    rarity:'MithiCal'
+  },
+]
 
 function App() {
   return (
@@ -72,12 +100,76 @@ function App() {
               <IoAdd style={{color:'orange', fontSize:20}}/> Add SQUID to Matemmark
             </div>
           </div>
-          <Product_Card/>
+          <div className='itemsbox'>
+          <div className='CardContiner'>
+            {
+              cards.map((item, index)=>{
+                return(
+                  <Product_Card
+                    image={item.picture}
+                    title={item.title}
+                    rarity={item.rarity}
+                  />
+                )
+              })
+            }
+          
+          </div>
+          <div className='earnedToken'>
+            <div style={{width:"180px"}}>
+            <div className='earnedtokentext'>
+              Earned Token
+            </div>
+            </div>
+            <div style={{width:"180px"}}>
+              <div className='balanecetext'>
+                10.0000
+              </div>
+            </div>
+            <div style={{width:"180px"}}>
+              <div className='earningtext'>
+                Earning <div style={{color:'#58f032', marginRight:'3px'}}>+0.1</div>  SQUID token daily
+              </div>
+            </div>
+            <div className='harvestbutton'>
+              <div style={{color:'black', fontSize:"medium", fontWeight:"800"}}>
+                Harvest
+              </div>
+
+            </div>
+          </div>
+          </div>
+          
         </div>
 
       </div>
       <div className="bottomView">
-        
+        <div style={{marginLeft:'30px', display:'flex'}}>
+          <img src={pic} style={{height:"120px", width:"130px"}}/>
+        </div>
+        <div style={{marginTop:40}}>
+          <div style={{color:'white', fontSize:'medium', fontWeight:'500'}}>
+            Contracts
+          </div>
+          <div style={{display:'flex', flexDirection:'row', marginTop:"30px"}}>
+            <div style={{color:'#d6670d', fontSize:"medium", fontWeight:'700', marginRight:"20px"}}>
+              SQUID Token
+            </div>
+            <div style={{color:'#d6670d', fontSize:"medium", fontWeight:'700', marginLeft:"20px"}}>
+              Guininfo
+            </div>
+          </div>
+
+        </div>
+        <div style={{color:'white', fontSize:28, marginRight:"30px", marginTop:"40px"}}>
+            <div style={{fontSize:"small", color:'white', fontWeight:'500'}}>
+              @2021 studio
+            </div>
+            <div style={{color:'#d6670d', fontSize:'small',}}>
+              Documentation
+            </div>
+
+        </div>
       </div>
 
     </div>
--- src/Components/Product_Card.js
+++ src/Components/Product_Card.js
@@ -1,8 +1,25 @@
 import React from 'react'
 import './ProductCard.css'
 
-export default function Product_Card() {
+export default function Product_Card({image, title, rarity}) {
   return (
-    <div className='card'>Product_Card</div>
+    <div className='card'>
+      <img src={image} style={{width:150, height:160, borderTopLeftRadius:5, borderTopRightRadius:5}}/>
+      <div className='desc'>
+        <div className='titletext'>
+          {title}
+        </div>
+        <div className='raritytext'>
+          Rarity: {rarity}
+        </div>
+        <div className='stakebutton'>
+          <div className='staketext'>
+            Stake
+          </div>
+
+        </div>
+      </div>
+      
+    </div>
   )
 }
